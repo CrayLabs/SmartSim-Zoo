940ab51500cf9f492a233d862b9bdd14bed3e311
Eric Gustin <34000337+EricGustin@users.noreply.github.com>
2022-02-25 12:36:08 -0800

    Add ThetaGPU installer script

--- thetagpu/smartsim_thetagpu_installer.sh
+++ thetagpu/smartsim_thetagpu_installer.sh
@@ -1,27 +1,28 @@
-#!/bin/bash
+#!/usr/bin/bash
 export PROJECT=datascience
 module load conda
-module load nvhpc-byo-compiler
-conda create --prefix /lus/theta-fs0/projects/$PROJECT/$USER/conda/envs/ss_env_gpu python=3.8 -y
+conda create --prefix /lus/theta-fs0/projects/$PROJECT/$USER/conda/envs/ss_env_gpu python=3.8 cmake git-lfs -y
 conda activate /lus/theta-fs0/projects/$PROJECT/$USER/conda/envs/ss_env_gpu
-pip install cmake
 cd /lus/theta-fs0/projects/$PROJECT/$USER
-mkdir smartsim-install-gpu
+mkdir -p smartsim-install-gpu
 cd smartsim-install-gpu
 git clone https://github.com/CrayLabs/SmartRedis.git smartredis
 git clone https://github.com/CrayLabs/SmartSim.git smartsim
 cd smartredis
 export CC=$(which gcc)
-export CXX=$(which gxx)
-make lib && pip install . numpy==1.19.5
+export CXX=$(which g++)
+make lib && pip install . numpy==1.19.5 \
+			  tensorflow==2.6.2 \
+			  onnx==1.9.0 \
+			  skl2onnx==1.10.3 \
+			  onnxmltools==1.10.0 \
+			  scikit-learn==1.0.2 \
+			  torch==1.9.1+cu111 \
+	          torchvision==0.10.1+cu111 \
+	    	  torchaudio==0.9.1 \
+	    	  -f https://download.pytorch.org/whl/torch_stable.html
 cd ..
 cd smartsim
-conda install swig cmake git-lfs -y
-conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch -y
-pip install . tensorflow==2.4.2 numpy==1.19.5 onnx==1.7
-export CUDNN_LIBRARY_DIR=/lus/theta-fs0/projects/$PROJECT/$USER/conda/envs/ss_env_gpu/lib
-export CUDNN_LIBRARY=/lus/theta-fs0/projects/$PROJECT/$USER/conda/envs/ss_env_gpu/lib
-export CUDNN_INCLUDE_DIR=/lus/theta-fs0/projects/$PROJECT/$USER/conda/envs/ss_env_gpu/include
-pip install .
-smart build --device gpu --onnx
-
+pip install -e .[dev,ray]
+export SMARTSIM_REDISAI=1.2.5
+smart build --device gpu --onnx -v --torch_dir=/lus/theta-fs0/projects/$PROJECT/$USER/conda/envs/ss_env_gpu/lib/python3.8/site-packages/torch/share/cmake/Torch/
